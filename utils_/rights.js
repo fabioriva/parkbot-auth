615e05ab4046d7b10c9e958bd328d2f936ee4dd2
Fabio Riva <f.riva@sotefin.ch>
2022-06-27 15:33:38 +0200

    add entry/exit/rollback rights

--- utils_/rights.js
+++ utils_/rights.js
@@ -1,11 +1,14 @@
-const ACTIONS = 'actions'
+const ACTIONS = 'actions' // deprecated
 const DIAGNOSTIC = 'diagnostic'
-const EDIT_CARD = 'edit-card'
+const EDIT_CARD = 'edit-card';
 const EDIT_STALL = 'edit-stall'
+const ENTRY = 'entry'
+const EXIT = 'exit'
+const ROLLBACK = 'rollback'
 
-const ADMIN_RIGHTS = [ACTIONS, DIAGNOSTIC, EDIT_CARD, EDIT_STALL]
-const SERVICE_RIGHTS = [ACTIONS, EDIT_CARD, EDIT_STALL]
-const VALET_RIGHTS = [ACTIONS]
+const ADMIN_RIGHTS = [ACTIONS, DIAGNOSTIC, EDIT_CARD, EDIT_STALL, ENTRY, EXIT, ROLLBACK]
+const SERVICE_RIGHTS = [EXIT, EDIT_CARD, EDIT_STALL]
+const VALET_RIGHTS = [EXIT]
 const USER_RIGHTS = []
 
 module.exports = {
